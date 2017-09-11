03d636ad7203be2464546b05f0b61c9736ca04be
aschwinwester <aschwin.wester@gmail.com>
2017-09-11 22:23:11 +0200

    Update Practice Exercise 2_1 (tuples).py
    commentaar op de code.

--- rolf1/les 2/Practice Exercise 2_1 (tuples).py
+++ rolf1/les 2/Practice Exercise 2_1 (tuples).py
@@ -1,7 +1,12 @@
+# moet een tuple zijn, geen array
 letters=['A', 'C', 'B', 'B', 'C', 'A', 'C', 'C', 'B']
 letters.sort()
 letters
+
+# dit statement heeft geen effect.
 ['A', 'A', 'B', 'B', 'B', 'C', 'C', 'C', 'C']
+
+# stop dit in een array ipv tuple.
 letters.count('A'),letters.count('B'),letters.count('C')
 (2, 3, 4)
 
