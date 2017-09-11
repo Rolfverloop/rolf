70cba862db7ab88a4b507723106fcac86c611e0a
aschwinwester <aschwin.wester@gmail.com>
2017-09-11 22:24:49 +0200

    Update Practice Exercise 2_2 (getallen, strings en conversie).py
    opmerkingen.

--- rolf1/les 2/Practice Exercise 2_2 (getallen, strings en conversie).py
+++ rolf1/les 2/Practice Exercise 2_2 (getallen, strings en conversie).py
@@ -2,11 +2,14 @@ cijferICOR=7
 cijferPROG=8
 cijferCNS=6
 gemiddelde=(cijferICOR+cijferPROG+cijferCNS)/3
+# gebruik een print statement.
 gemiddelde
 7.0
 beloning=(cijferICOR+cijferPROG+cijferCNS)*30
 beloning
 630
+
+# dit is niet zoals verwacht.
 overzicht='ik haalde een gemiddelde van',(gemiddelde),'en verdiende hiermee',beloning
 overzicht
 ('ik haalde een gemiddelde van', 7.0, 'en verdiende hiermee', 630)
